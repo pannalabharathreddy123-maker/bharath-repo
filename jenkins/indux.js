72ad930c37b29d30216cdcd8ef01eb42751218ff
root <root@k8s-kind-vm>
2025-10-31 14:02:46 +0000

    add jenkins file

--- jenkins/indux.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello from node-sample!'));
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('listening on', port));
-
